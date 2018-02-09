75db8d2f612bfa7b89f355052c1621b569eca9d8
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-09 08:47:16 -0500

    Updated from Brython Server: 2/9/2018 8:47:15 AM

--- name.py
+++ name.py
@@ -1,1 +1,19 @@
+#Shaylee McBride
+#9Feb2018
+#name.py - input name & fav color, get cool screen
 
+from ggame import *
+
+name = input("What is your name? ")
+color = input("What is your favorite color's hex code (no 0x)? ")
+
+fillColor = Color('0x'+color,1)
+
+outline = LineStyle(0,fillColor)
+
+rectangle = RectangleAsset(1080,580,outline,fillColor)
+text = TextAsset(name,fill=Color(0x000000,1),style="bold 50pt Courier")
+
+Sprite(rectangle)
+Sprite(text,(350,250))
+App().run()
