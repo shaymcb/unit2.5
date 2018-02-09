d43099975ec2b5f7f8ca4634f236b8f3ecb057ea
shaymcb <35492782+shaymcb@users.noreply.github.com>
2018-02-09 08:37:24 -0500

    Updated from Brython Server: 2/9/2018 8:37:23 AM

--- germany.py
+++ germany.py
@@ -18,5 +18,5 @@ text = TextAsset("WILLKOMMEN IN DEUTSCHLAND",fill=black,style="bold 50pt Times",
 Sprite(blackStripe)
 Sprite(redStripe,(0,100))
 Sprite(yellowStripe,(0,200))
-Sprite(text)
+Sprite(text,(0,300))
 App().run()
